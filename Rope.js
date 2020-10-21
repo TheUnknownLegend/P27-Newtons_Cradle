91f5ef829addef953f90aaaa34d38ae554e5a720
TheUnknownLegend <71276437+TheUnknownLegend@users.noreply.github.com>
2020-10-21 04:41:22 -0700

    Add files via upload

--- Rope.js
+++ Rope.js
@@ -1,10 +1,12 @@
 class rope{
     constructor(body1,body2,offsetX,offsetY){
+
+        this.offsetX = offsetX
+        this.offsetY = offsetY
+
         var options = {
             body1: body1,
             body2: body2,
-            offsetX : offsetX,
-            offsetY : offsetY,
             pointB: {x:this.offsetX, y:this.offsetY}
         }
 
